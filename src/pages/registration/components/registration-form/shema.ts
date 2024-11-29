027ee0732f7e454d3841e1105b9dd0ee3bff8439
giorgi <zautashvili1994@gmail.com>
2024-11-29 19:54:22 +0400

    add email pattern to zod

--- src/pages/registration/components/registration-form/shema.ts
+++ src/pages/registration/components/registration-form/shema.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const registerFormSchema = z.object({
-  email: z.string().min(10).max(50),
+  email: z.string().min(10).max(50).email(),
   password: z.string().min(6).max(50),
   confirmPassword: z.string().min(6).max(50),
 });
