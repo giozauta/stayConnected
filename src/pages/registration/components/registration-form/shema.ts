5df8d7a63be8380fd231e30172a3bfbe55487015
giorgi <zautashvili1994@gmail.com>
2024-11-30 22:31:59 +0400

    add name lastname inputs

--- src/pages/registration/components/registration-form/shema.ts
+++ src/pages/registration/components/registration-form/shema.ts
@@ -1,6 +1,8 @@
 import { z } from "zod";
 
 export const registerFormSchema = z.object({
+  name: z.string().min(2).max(50),
+  lastName: z.string().min(2).max(50),
   email: z.string().min(10).max(50).email(),
   password: z.string().min(8).max(50),
   confirmPassword: z.string().min(8).max(50),
