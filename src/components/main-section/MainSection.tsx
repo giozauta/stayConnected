ae90d8e67792c94c35bf5a7d1da1aa06e80e49c0
giorgi <zautashvili1994@gmail.com>
2024-12-01 15:01:42 +0400

    change mainSection style

--- src/components/main-section/MainSection.tsx
+++ src/components/main-section/MainSection.tsx
@@ -3,8 +3,9 @@ import { PropsWithChildren } from "react";
 const MainSection: React.FC<PropsWithChildren> = ({ children }) => {
   return (
     <div
-      className="h-screen
-        flex items-center justify-center"
+      className="
+        p-10
+        flex items-center justify-center bg-red-300"
     >
       {children}
     </div>
